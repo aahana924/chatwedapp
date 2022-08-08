def0208c712e4c3f02a48e3b449ccc2f4b98aeac
aahana924 <87435688+aahana924@users.noreply.github.com>
2022-08-08 18:38:15 +0530

    Add files via upload

--- kwitter.js
+++ kwitter.js
@@ -2,4 +2,5 @@ var username ="";
 function send(){
      username = document.getElementById("username").value;
     localStorage.setItem("pinky",username);
+    window.location="Kwitter_room.html"
 }
